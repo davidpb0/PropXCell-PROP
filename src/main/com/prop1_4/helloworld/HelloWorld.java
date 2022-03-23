95fbaf9489f8c4f426ba761617fb7e54282df917
QuimTorra <q.torra@gmail.com>
2022-03-23 11:46:21 +0100

    Prova Tests Creat un nou directori "test" Afegit un mètode suma a la classe HelloWorld Afegida una classe HelloWorldTest Afegits dos tests per al mètode Suma

--- src/main/com/prop1_4/helloworld/HelloWorld.java
+++ src/main/com/prop1_4/helloworld/HelloWorld.java
@@ -4,4 +4,13 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Soy un master de git");
     }
+
+    /**
+     * @param a = integer
+     * @param b = integer
+     * @return sum of a and b
+     */
+    public static int suma (int a, int b) {
+        return a +b;
+    }
 }
