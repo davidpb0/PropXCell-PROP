3798b9493c3d6ee81faa77e479e6c05a01d99d9f
ivan <ivan.risueno@estudiantat.upc.edu>
2022-03-23 11:15:04 +0100

    segon canvi(chorrada)

--- src/main/com/prop1_4/helloworld/HelloWorld.java
+++ src/main/com/prop1_4/helloworld/HelloWorld.java
@@ -2,6 +2,6 @@ package main.com.prop1_4.helloworld;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Bye bye world :(");
+        System.out.println("Soy un master de git");
     }
 }
