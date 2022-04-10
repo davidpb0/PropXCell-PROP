b9b7868d84c661205026d9312d0f9543bfcd699a
david <david.perez.barroso@estudiantat.upc.edu>
2022-04-10 14:55:00 +0200

    ControladorCelda:: FuncionesConversiones + CorrecciónTruncarValor() + ControladorCeldaTest::Mejora truncarValor()

--- src/DomainControllers/ControladorCelda.java
+++ src/DomainControllers/ControladorCelda.java
@@ -38,13 +38,27 @@ public class ControladorCelda {
      * @return Un string con _value truncado a tantos decimales como se ha seleccionado en _op
      */
 
-   public void truncarValor(int _op){
+   public void truncarValor(int _op) {
        float v = Float.parseFloat(this.celdaRef.getValor());
 
-       int indice = (int) Math.pow(10, _op);
-       v = (int)(v*indice);
-       v = (float)(v/indice);
-       celdaRef.setValor(String.valueOf(v));
+       String str = String.valueOf(Math.abs(v));
+
+       //Cojo la parte entera del numero
+       int n = Integer.parseInt(str.substring(0, str.indexOf('.')));
+
+       //Cojo la parte decimal del numero y la transformo en un entero
+       int dn = Integer.parseInt(str.substring(str.indexOf('.') + 1));
+
+       //Calculo los digitos que tiene cada uno y los sumo para saber el total
+       int dig = ((int)(Math.log10(n)+1)) + ((int)(Math.log10(dn)+1));
+
+       if (dig >= _op && _op >= 0) {
+           int indice = (int) Math.pow(10, _op);
+           v = (int) (v * indice);
+           v = (float) (v / indice);
+           celdaRef.setValor(String.valueOf(v));
+       }
+       else celdaRef.setValor("#ERROR");
 
     }
 
@@ -68,6 +82,62 @@ public class ControladorCelda {
         celdaRef.setValor(String.valueOf(v));
 
     }
+
+    /**
+     * Covierte el valor decimal de la celda a binario
+     */
+    public void convertirValorDB(){
+        int dec = Integer.parseInt(this.celdaRef.getValor());
+        this.celdaRef.setValor(Integer.toBinaryString(dec));
+    }
+
+    /**
+     * Convierte el valor binario de la celda a decimal
+     */
+    public void convertirValorBD(){
+        int decConv = Integer.parseInt(this.celdaRef.getValor(), 2);
+        this.celdaRef.setValor(String.valueOf(decConv));
+    }
+
+    /**
+     * Convierte el valor decimal de la celda a hexadecimal
+     */
+    public void convertirValorDH(){
+        int dec = Integer.parseInt(this.celdaRef.getValor());
+        this.celdaRef.setValor(Integer.toHexString(dec));
+    }
+
+
+    /**
+     * Convierte el valor hexadecimal de la celda a decimal
+     */
+    public void convertirValorHD(){
+        int decConv = Integer.parseInt(this.celdaRef.getValor(), 16);
+        this.celdaRef.setValor(String.valueOf(decConv));
+    }
+
+    /**
+     * Convierte el valor binario de la celda en hexadecimal
+     */
+    public void convertirValorBH(){
+        int decConv = Integer.parseInt(this.celdaRef.getValor(), 2);
+        this.celdaRef.setValor(Integer.toHexString(decConv));
+    }
+
+
+    /**
+     * Convierte el valor hexadeciaml de la celda en binario
+     */
+    public void convertirValorHB(){
+        int decConv = Integer.parseInt(this.celdaRef.getValor(), 16);
+        this.celdaRef.setValor(Integer.toBinaryString(decConv));
+    }
+
+
+
+
+
+
 
 
 }
--- test/DomainControllers/ControladorCeldaTest.java
+++ test/DomainControllers/ControladorCeldaTest.java
@@ -18,8 +18,30 @@ public class ControladorCeldaTest {
         d.getHoja(1).getCelda(1,2).setValor("5.5678945");
         cc.truncarValor(3);
         String s = d.getHoja(1).getCelda(1,2).getValor();
+
+        cc.asignaCeldaPosicion(1, 1, 1);
+        d.getHoja(1).getCelda(1,1).setValor("5.5678945");
+        cc.truncarValor(4);
+        String s2 = d.getHoja(1).getCelda(1,1).getValor();
+
+        cc.asignaCeldaPosicion(1, 2, 1);
+        d.getHoja(1).getCelda(2,1).setValor("5.5678945");
+        cc.truncarValor(0);
+        String s3 = d.getHoja(1).getCelda(2,1).getValor();
+
+        cc.asignaCeldaPosicion(1, 2, 2);
+        d.getHoja(1).getCelda(2,2).setValor("5.5678945");
+        cc.truncarValor(10);
+        String s4 = d.getHoja(1).getCelda(2,2).getValor();
+        System.out.println(s4);
+
         assertEquals("5.567", s);
+        assertEquals("5.5678", s2);
+        assertEquals("5.0", s3);
+        assertEquals("#ERROR", s4);
 
     }
+
+
 
 }
