6400896f934828acb8af065404cf50288d514136
ivan <ivan.risueno@estudiantat.upc.edu>
2022-04-16 18:20:24 +0200

    Primera versión completa de ControladorBloque
    Faltaba añadir el método pegar() y hacer las descripciones de los métodos.

--- src/DomainControllers/ControladorBloque.java
+++ src/DomainControllers/ControladorBloque.java
@@ -1,4 +1,11 @@
 package DomainControllers;
+/*
+ * ClassName DomainControllers.ControladorBloque
+ *
+ * Version info 0.1.0
+ *
+ * Author Iván Risueño Martín
+ */
 
 import DomainModel.*;
 
@@ -6,18 +13,38 @@ public class ControladorBloque {
 
     private BloqueTemporalCopiado bloqueCopiado;
 
+    /**
+     * Da de alta un BloqueTemporalCopiado para copiarlo a otro lugar del documento
+     */
     public void copiar() {
         bloqueCopiado = new BloqueTemporalCopiado();
         bloqueCopiado.setCortar(false);
     }
 
+    /**
+     * Da de alta un BloqueTemporalCopiado para cortarlo a otro lugar del documento
+     */
     public void cortar() {
         bloqueCopiado = new BloqueTemporalCopiado();
         bloqueCopiado.setCortar(true);
     }
 
-    public void pegar(int _numHoja, int _FilaInicio, int _filaInicio, int _columnaInicio) {
+    /**
+     * Pega el BloqueTemporalCopiado en un cierto lugar del documento
+     * @param _numHoja identificador de la hoja destino
+     * @param _filaInicio identificador de la fila destino
+     * @param _columnaInicio identificador de la columna destino
+     */
+    public void pegar(int _numHoja, int _filaInicio, int _columnaInicio) {
         Hoja h = Documento.getDocumento().getHoja(_numHoja);
-        // Falta la operacion de Hoja para reemplazar una celda por otra
+        for (int f = 1; f <= bloqueCopiado.getTamanoFilas(); ++f) {
+            for (int c = 1; c <= bloqueCopiado.getTamanoColumnas(); ++c) {
+                Celda cell = bloqueCopiado.getCelda(f, c);
+                h.cambiarPosicionCelda(new Posicion(_filaInicio + f - 1, _columnaInicio + c - 1), cell);
+            }
+        }
+        if (bloqueCopiado.getCortar()) {
+            bloqueCopiado = null;
+        }
     }
 }
