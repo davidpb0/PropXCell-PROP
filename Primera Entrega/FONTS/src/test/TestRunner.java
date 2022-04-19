95c65f65748bc4bf0594c35855c2c772ae976594
ivan <ivan.risueno@estudiantat.upc.edu>
2022-04-19 18:53:31 +0200

    Test Runner

---
+++ Primera Entrega/FONTS/src/test/TestRunner.java
@@ -0,0 +1,19 @@
+package test;
+
+import org.junit.runners.Suite;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.RunWith;
+
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        JUnitTestSuite.class
+})
+
+public class TestRunner {
+    public static void main(String[] args) {
+        Result result = JUnitCore.runClasses(JUnitTestSuite.class);
+        System.out.println("Tests ejecutados: " + result.getRunCount() + "\nTests ejecutados correctamente: " + (result.getRunCount() - result.getFailureCount()) + "\nTests fallidos: " + result.getFailureCount());
+    }
+}
