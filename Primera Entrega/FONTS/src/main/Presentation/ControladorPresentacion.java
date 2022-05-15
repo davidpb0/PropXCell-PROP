a11180af0840ca32be3b05a6121a25ef5ff10557
ivan <ivan.risueno@estudiantat.upc.edu>
2022-05-15 15:04:45 +0200

    Controlador de presentacion v 0.0.2

--- Primera Entrega/FONTS/src/main/Presentation/ControladorPresentacion.java
+++ Primera Entrega/FONTS/src/main/Presentation/ControladorPresentacion.java
@@ -2,14 +2,18 @@ package main.Presentation;
 /*
  * ClassName DomainControllers.ControladorPresentacion
  *
- * Version info 0.0.1
+ * Version info 0.0.2
  *
  * Author Iván Risueño Martín
  */
+
 import main.Domain.DomainControllers.ControladorDominio;
+import main.Presentation.vistas.PantallaInicial.PantallaInicial;
+import main.Presentation.vistas.PantallaPrincipal.PantallaPrincipal;
 
 public class ControladorPresentacion {
     private final ControladorDominio contDominio;
+    private PantallaInicial pIni;
 
     public ControladorPresentacion() {
         this.contDominio = ControladorDominio.getControladorDominio();
@@ -17,5 +21,15 @@ public class ControladorPresentacion {
 
     public ControladorDominio getControladorDominio() {
         return contDominio;
+    }
+
+    public PantallaPrincipal getPantallaPrincipal() {
+        return new PantallaPrincipal(this.contDominio);
+    }
+
+    public void iniciaPInicial() {
+        this.pIni = new PantallaInicial();
+        pIni.inicializaVista();
+        pIni.setVisible(true);
     }
 }
